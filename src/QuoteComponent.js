4f733d2cbbbf2c36130615699fcc46aa25e10eb9
Heemo Yang <heemo521@gmail.com>
2022-02-23 14:00:06 -0800

    Update Before Styling

--- src/QuoteComponent.js
+++ src/QuoteComponent.js
@@ -1,12 +1,13 @@
 import { useState, useEffect } from 'react';
 import { Button, Card, CardBody, CardTitle, CardText, CardFooter } from 'reactstrap';
+// import classes from './QuoteComponent.module.css';
 
-// const quotes = [];
-// const quote = {
-//     _id: '',
-//     content: '',
-//     author: '',
-// };
+// Adding CSS Styling
+// Add Social Media button to share on twitter
+// Auto-change quotes
+
+//User Story #5: Within #quote-box, I can see a clickable a element with a corresponding id="tweet-quote".
+// User Story #10: I can tweet the current quote by clicking on the #tweet-quote a element. This a element should include the "twitter.com/intent/tweet" path in its href attribute to tweet the current quote.
 
 const QuoteComponent = (props) => {
     const [quoteData, setQuoteData] = useState(null);
@@ -39,54 +40,22 @@ const QuoteComponent = (props) => {
                         <i class="fa fa-quote-left"> </i>
                         <div id="text">{quoteData.content}</div>
                     </CardText>
-                    <CardTitle id="author">-{quoteData.author}</CardTitle>
+                    <CardTitle id="author">- {quoteData.author}</CardTitle>
                 </CardBody>
                 <CardFooter className="buttons">
-                    <div id="tweet-quote">
-                        <Button className="button" color="primary" href="twitter.com/intent/tweet">
-                            <i className="fa fa-twitter"></i>Social Media Buttons
-                        </Button>
+                    <div>
+                        <a className="button" color="primary" href="twitter.com/intent/tweet" id="tweet-quote">
+                            <i className="fa fas-twitter"></i>
+                            Social Media Buttons
+                        </a>
                     </div>
                     <Button id="new-quote" color="primary" onClick={fetchQuote}>
                         New quote button
                     </Button>
                 </CardFooter>
             </Card>
-            {/* <div className="row">
-                <div>
-                    
-                </div>
-                <div id="author">- Author Name</div>
-            </div>
-            <div className="row">
-                <div id="tweet-quote">
-                    <a href="twitter.com/intent/tweet">
-                        <i className="fa fa-twitter" />
-                    </a>
-                    Social Media Buttons
-                </div>
-                <Button id="new-quote">New quote button</Button>
-            </div> */}
         </div>
     );
 };
 
 export default QuoteComponent;
-
-// User Story #6: On first load, my quote machine displays a random quote in the element with id="text".
-
-// User Story #7: On first load, my quote machine displays the random quote's author in the element with id="author".
-
-// User Story #8: When the #new-quote button is clicked, my quote machine should fetch a new quote and display it in the #text element.
-
-// User Story #9: My quote machine should fetch the new quote's author when the #new-quote button is clicked and display it in the #author element.
-
-// User Story #10: I can tweet the current quote by clicking on the #tweet-quote a element. This a element should include the "twitter.com/intent/tweet" path in its href attribute to tweet the current quote.
-
-// User Story #11: The #quote-box wrapper element should be horizontally centered. Please run tests with browser's zoom level at 100% and page maximized.
-
-// You can build your project by using this CodePen template and clicking Save to create your own pen. Or you can use this CDN link to run the tests in any environment you like: https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js
-
-// Once you're done, submit the URL to your working project with all its tests passing.
-
-// Note: Twitter does not allow links to be loaded in an iframe. Try using the target="_blank" or target="_top" attribute on the #tweet-quote element if your tweet won't load. target="_top" will replace the current tab so make sure your work is saved.
